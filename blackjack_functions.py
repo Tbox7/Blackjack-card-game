acd684294b796a4e834ac1dc137525f66f822153
Tb0x <60563088+Tbox7@users.noreply.github.com>
2025-02-17 22:10:46 -0600

    Update blackjack_functions.py

--- blackjack_functions.py
+++ blackjack_functions.py
@@ -1,85 +1,81 @@
 import os
 import random
-import blackjack_art
-cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+import blackjack_art 
+import time
+cards = [11,11,11,11,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,]
 dealer_hand = []
 game_bets = {}
 players ={}
-
+game = False
+turn_over = False
 def reset():
     """Resets all variables to 0"""
-    game_bets = {}
-    dealer_hand = []
+    dict.clear(game_bets)
+    list.clear(dealer_hand)
     for i in players:
         players[i]['cards'] = []
 
-def doube_down(player):
-    """This function is to double the money you have bet"""
-    new_card = random.choice(cards)
-    bet = game_bets[player]
-    players[player]['coins'] -= bet
-    bet += bet
-    game_bets[player] = bet
-    return new_card
-
 def dealer_turn():
     """This is gonna be the hit for the dealer | this will have a soft 17"""
-    #delete if i can make the hit() function also work with the dealer list. 
-    #return the number value of the dealer
-    #if dealer goes over return 0
-    x = 0
-    for i in dealer_hand:
-        x += i
-        if x == 21:
-            return x
-        elif x>21:
-            return 0
-        elif x<17:
-            dealer_hand.append(random.choice(cards))
-        else:
-            return x
-    
-def print_hand(num):
+    dealer_hand = []
+    result = 0
+    for value in dealer_hand:
+       result += value
+    while result <=17:
+        card_value = random.choice(cards)
+        dealer_hand.append(card_value)
+        result += card_value
+    if (result >= 21 or result >= 17) and 11 in dealer_hand:
+        dealer_hand = [1 if x == 11 else x for x in dealer_hand]
+        dealer_turn()
+    return result
+
+def print_hand(player):
     """This will print the current hand of the player"""
-    for i in players[num]['cards']:
-        if i == 10: 
-            print(blackjack_art.random.choice(blackjack_art.cards[10]))
-        if i != 10:   
-            print(blackjack_art.cards[i])
+    for i in players[player]['cards']:
+        print(blackjack_art.cards[i])
 
 def win(player):
     """This function computes when you beat dealer"""
     bet = game_bets[player]
-    bet = bet+bet
-    players[player]['coins'] += bet
+    bet *=2
+    curr_coins = int(players[player]['coins']) 
+    curr_coins+= bet
+    players[player]['coins'] = str(curr_coins)
 
 def blackjack(player):
     """This funciton computes when you get a blackjack (perfect 21)"""
     bet = game_bets[player]
-    bet = bet+bet+bet
-    players[player]['coins'] += bet
-
-def push(player):
-    bet = game_bets[player]
-    players[player]['coins'] += bet
+    bet *=3
+    curr_coins = int(players[player]['coins']) 
+    curr_coins+= bet
+    players[player]['coins'] = str(curr_coins)
 
 def print_dealer_hand():
     print("<><><><><><><><><><><><><><><><><><><>")
-    print("<><><><><> DEALER HAND <><><><><><>")
+    print("<><><><><><> DEALER HAND <><><><><><>")
     print("<><><><><><><><><><><><><><><><><><><>")
-    if dealer_hand[0] != 10:
-        print(blackjack_art.cards[dealer_hand[0]])
-    elif dealer_hand == 10:
-        print(blackjack_art.random.choice(blackjack_art.cards[10]))
+    for card in dealer_hand:
+        print(blackjack_art.cards[card])
     print("<><><><><><><><><><><><><><><><><><><>")
 
-def hit():
+def dealer_hand_hidden():
+    print("<><><><><> DEALER HAND <><><><><><>")
+    print("<><><><><><><><><><><><><><><><><><><>")
+    print(blackjack_art.cards[dealer_hand[1]])
+    print(blackjack_art.blank)
+    print("<><><><><><><><><><><><><><><><><><>")
+    print("<><><><> LETS GET STARTED <><><><><>") 
+    print("<><><><><><><><><><><><><><><><><><>")
+
+def hit(player):
     """This funciton deals a card to player"""
     new_card = random.choice(cards)
+    players[player]['cards'].append(new_card)
     clear()
     print(blackjack_art.logo)
-    print(print_dealer_hand())
-    return new_card
+    dealer_hand_hidden()
+    print_player_hand(player)
 
 def dealer():
     """This function is the dealer and its calculations"""
@@ -87,37 +83,24 @@ def dealer():
     two = int(random.choice(cards))
     dealer_hand.append(one)
     dealer_hand.append(two)
-    print("<><><><><> DEALER HAND <><><><><><>")
-    print("<><><><><><><><><><><><><><><><><><><>")
-    if one == 10 and two != 10:
-        print(blackjack_art.random.choice(blackjack_art.cards[10]))
-    if two == 10 and one != 10:
-        print(blackjack_art.cards[one])
-    if two != 10 and one != 10:   
-        print(blackjack_art.cards[one])
-    if two == 10 and one == 10:
-        print(blackjack_art.random.choice(blackjack_art.cards[10]))
-    print(blackjack_art.blank)
-    print("<><><><><><><><><><><><><><><><><><>")
-    print("<><><><> LETS GET STARTED <><><><><>") 
-    print("<><><><><><><><><><><><><><><><><><>")
-
+    dealer_hand_hidden()
+    
 def create_player(num):
     """This function creates a player with input(name)"""
-    emp = {}
     for i in range(1,num+1):
-        emp['cards'] =[]
-        emp["coins"] = 1000
-        players['player'+str(i)] = emp
-        emp= {}
+        temp_name = ('player'+str(i)) 
+        temp_name = {}  
+        temp_name['cards'] =[]
+        temp_name["coins"] = 1000
+        players['player'+str(i)] = temp_name
 
 def setup():
     """This function will setup the game with the inital bids"""
-    for i in players:
+    for i in (players):
         print(f"{i} you have {players[i]['coins']}.")
         bet = int(input(f'{i} bet: '))
         players[i]['coins'] = players[i]['coins'] - bet
-        game_bets[str(i)] = (bet)
+        game_bets[str(i)] = (f" has bet {bet}")
     clear()
     print(blackjack_art.logo)
     print(game_bets)
@@ -129,23 +112,48 @@ def deal():
         two = int(random.choice(cards))
         players[i]['cards'].append(one)
         players[i]['cards'].append(two)
-        print("<><><><><><><><><><><><><><><><><><>")
-        print(i)
-        if one == 10 and two != 10:
-            print(blackjack_art.random.choice(blackjack_art.cards[10]))
-            print(blackjack_art.cards[two])
-        if two == 10 and one != 10:
-            print(blackjack_art.cards[one])
-            print(blackjack_art.random.choice(blackjack_art.cards[10]))
-        if two != 10 and one != 10:   
-            print(blackjack_art.cards[one])
-            print(blackjack_art.cards[two])
-        if two == 10 and one == 10:
-            print(blackjack_art.random.choice(blackjack_art.cards[10]))
-            print(blackjack_art.random.choice(blackjack_art.cards[10]))
-    print("<><><><><><><><><><><><><><><><><><>")
 
 def clear():
     """clears terminal"""
     os.system('cls' if os.name=='nt' else 'clear')
     return("   ")
+
+def player_action(player):
+    """This function will take the player input and do the action"""
+    result = 0
+    turn_over = False
+    player_status = ''
+    for card in players[player]['cards']:
+        result += card
+    if result == 21:
+        turn_over = True
+        player_status = 'BLACKJACK'
+        return turn_over, player_status, result
+    elif result >=21:
+        turn_over = True
+        player_status = 'BUST'
+        return turn_over, player_status, result
+    else:
+        ans = input(f"{player} What would you like to do? |'h' is to hit|'s' is to stay:  ")
+        if ans == 'h':
+            hit(player)
+            player_action(player)
+        elif ans == 's':
+            turn_over = True
+            player_status = 'STAY'
+        return turn_over, player_status, result
+    
+
+def print_player_hand(player):
+    """This function will print the player hand"""
+    print("<><><><><><><><><><><><><><><><><><>")
+    print((f"{player} has: "))
+    for i in players[player]['cards']:
+        print(blackjack_art.cards[i])
+    print("<><><><><><><><><><><><><><><><><><>")
+
+
+def countdown(seconds):
+    while seconds:
+        time.sleep(1)
+        seconds -= 1
